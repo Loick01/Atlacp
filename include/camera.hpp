36eae1d4e27b9f0e545d7a5188de4b71b92469f6
Loick01 <loicker89@gmail.com>
2025-12-11 16:27:21 +0100

    Use viewport instead of window size to fix culling problem

--- include/camera.hpp
+++ include/camera.hpp
@@ -9,24 +9,23 @@ class Camera
 {
     private:
         ScenePosition m_position;
-        const ScenePosition m_window_size;
-        ScenePosition m_viewport; // How many tiles (width and height) will be rendered in the window. Should not be a ScenePosition
+        ScenePosition m_range_tile; // How many tiles (width and height) will be rendered in the window. Should not be a ScenePosition
+        ScenePosition m_viewport; // Size of rendering area
         
         ScenePosition m_tilemap_size; // Could change when loading a new map
         Pair<bool> m_is_off_screen;
         float m_zoom;
 
     public:
-        Camera(const ScenePosition window_size, const ScenePosition viewport, const int tile_size);
+        Camera(const ScenePosition window_size, const ScenePosition range_tile, const int tile_size);
         ~Camera();
 
         ScenePosition GetCameraPosition() const;
-        ScenePosition GetWindowSize() const;
-        ScenePosition GetViewport() const;
+        ScenePosition GetRangeTile() const;
         Pair<bool> GetIsOffScreen() const;
         float GetZoom() const;
         void AddZoom(const float z);
-        void SetTilemapInfo(const ScenePosition tilemap_size);
+        void SetTilemapInfo(const ScenePosition tilemap_size, const int tile_size);
         void SetCameraPosition(const ScenePosition sp); // Set m_position to sp
         void MoveCameraPosition(const ScenePosition sp); // Add sp to m_position
         void Reset();
--- src/camera.cpp
+++ src/camera.cpp
@@ -1,12 +1,13 @@
 #include "camera.hpp"
 
-Camera::Camera(const ScenePosition window_size, const ScenePosition viewport, const int tile_size):
-    m_window_size(window_size), m_viewport(viewport)
+Camera::Camera(const ScenePosition window_size, const ScenePosition range_tile, const int tile_size):
+    m_range_tile(range_tile)
 {
     m_position = ScenePosition{0,0};
-    const Pair<float> best_possible_zoom = {(window_size.x/viewport.x)/static_cast<float>(tile_size),
-                                            (window_size.y/viewport.y)/static_cast<float>(tile_size)};
+    const Pair<float> best_possible_zoom = {(window_size.x/range_tile.x)/static_cast<float>(tile_size),
+                                            (window_size.y/range_tile.y)/static_cast<float>(tile_size)};
     m_zoom = std::min(best_possible_zoom.x, best_possible_zoom.y);
+    m_viewport = m_range_tile*tile_size*m_zoom;
 }
 
 Camera::~Camera()
@@ -19,14 +20,9 @@ ScenePosition Camera::GetCameraPosition() const
     return m_position;
 }
 
-ScenePosition Camera::GetWindowSize() const
+ScenePosition Camera::GetRangeTile() const
 {
-    return m_window_size;
-}
-
-ScenePosition Camera::GetViewport() const
-{
-    return m_viewport;
+    return m_range_tile;
 }
 
 Pair<bool> Camera::GetIsOffScreen() const
@@ -44,10 +40,10 @@ void Camera::AddZoom(const float z)
     m_zoom += z;
 }   
 
-void Camera::SetTilemapInfo(const ScenePosition tilemap_size)
+void Camera::SetTilemapInfo(const ScenePosition tilemap_size, const int tile_size)
 {
     m_tilemap_size = tilemap_size;
-    m_is_off_screen = m_tilemap_size > m_window_size;
+    m_is_off_screen = m_tilemap_size > m_viewport;
 }
 
 void Camera::SetCameraPosition(const ScenePosition sp)
@@ -69,10 +65,10 @@ void Camera::Reset() // Used in editor
 // Should use 2 different version of this : x axis / y axis
 void Camera::LookAt(const ScenePosition sp) // Center the camera on a scene position with checking map bounds
 {
-    ScenePosition camera_position = sp - m_window_size/2;
-    if (m_is_off_screen.x) camera_position.x = std::clamp(camera_position.x, 0, m_tilemap_size.x-m_window_size.x); // Map must be render at ScenePosition{0,0}
-    else camera_position.x = m_tilemap_size.x/2 - m_window_size.x/2;
-    if (m_is_off_screen.y) camera_position.y = std::clamp(camera_position.y, 0, m_tilemap_size.y-m_window_size.y); // Map must be render at ScenePosition{0,0}
-    else camera_position.y = m_tilemap_size.y/2 - m_window_size.y/2;
+    ScenePosition camera_position = sp - m_viewport/2;
+    if (m_is_off_screen.x) camera_position.x = std::clamp(camera_position.x, 0, m_tilemap_size.x - m_viewport.x); // Map must be render at ScenePosition{0,0}
+    else camera_position.x = m_tilemap_size.x/2 - m_viewport.x/2;
+    if (m_is_off_screen.y) camera_position.y = std::clamp(camera_position.y, 0, m_tilemap_size.y - m_viewport.y); // Map must be render at ScenePosition{0,0}
+    else camera_position.y = m_tilemap_size.y/2 - m_viewport.y/2;
     SetCameraPosition(camera_position);
 }
